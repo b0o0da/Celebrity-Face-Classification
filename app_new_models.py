a038678e6bb87d868f2650f0832a4cded34047d3
Abdelrahman Arafa <abdelrahmanarafaaa@gmail.com>
2025-12-30 00:33:42 +0200

    Update app_new_models.py

--- app_new_models.py
+++ app_new_models.py
@@ -33,7 +33,7 @@ for name, filename in model_files.items():
     model_path = hf_hub_download(
         repo_id=repo_id,
         filename=filename,
-        token="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"  
+        token="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"  
     )
     loaded_models[name] = load_model(model_path)
 
